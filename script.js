89776bf0d0854fd5f74734e678f5c094cc578d5f
Samuel Kyletoft <skyletoft@gmail.com>
2019-09-22 00:16:29 +0200

    Hopefully considered functioning?

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
 let riddleNumber = 0;
 
-init();
-
 function init () {
-    if (localStorage.riddleNumber == undefined) {
-        console.log("this is valid JS");
+    if (localStorage.riddleNumber !== undefined) {
+        riddleNumber = parseInt(localStorage.riddleNumber);
     }
+    questions[riddleNumber].generateText();
 }
+
+function checkAnswer () {
+    if (questions[riddleNumber].validateAnswer()) {
+        console.log("CORRECT!");
+        riddleNumber = questions[riddleNumber].nextQuestion;
+        localStorage.riddleNumber = riddleNumber;
+        questions[riddleNumber].generateText();
+    } else {
+        console.log("FALSE");
+    }
+    document.getElementById("inputbox").value = "";
+}
+
+
+const questions = [
+    {
+        generateText: function () {
+            document.getElementById("questionLabel").innerHTML = this.question;
+            document.getElementById("inputbox").setAttribute("type", "text");
+        },
+        validateAnswer: function () {
+            return document.getElementById("inputbox").value === this.answer;
+        },
+        question: "Vad säger morsen? [LÄNK]",
+        answer: "HEJ",
+        nextQuestion: 1
+    },
+    {
+        generateText: function () {
+            document.getElementById("questionLabel").innerText = "Ladda upp en bild!";
+            document.getElementById("inputbox").setAttribute("type", "file");
+        },
+        validateAnswer: function () {
+            const imageformats = [".png", ".jpg", "jpeg", ".gif", "tiff", ".bmp"]
+            let text = document.getElementById("inputbox").value.toString();
+            text = text.substr(text.length - 4);
+            console.log(text);
+            if (imageformats.indexOf(text) != -1) {
+                return true;
+            }
+            return false;
+        },
+        nextQuestion: 2
+    },
+    {
+        generateText: function () {
+            document.getElementById("questionLabel").innerHTML = "Hur många trianglar?<br><img src='https://1.bp.blogspot.com/-5xv6Cg1yhFo/VNLXEhuYiXI/AAAAAAAAAXw/dNgNTrE4G5M/s1600/image002.gif' width='250'/>"
+            document.getElementById("inputbox").setAttribute("type", "text");
+        },
+        validateAnswer: function () {
+            if (document.getElementById("inputbox").value === "27") {
+                return true;
+            }
+            return false;
+        },
+        nextQuestion: 0
+    }
+];
