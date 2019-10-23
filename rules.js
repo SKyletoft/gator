d9dfac49af41ba367fc027ff95d68ce8bbbb7634
SKyletoft <45131343+SKyletoft@users.noreply.github.com>
2019-10-23 13:08:05 +0200

    Update rules.js
    Moved questions out of here into its own file

--- rules.js
+++ rules.js
@@ -4,7 +4,24 @@ function init () {
     if (localStorage.riddleNumber !== undefined) {
         riddleNumber = parseInt(localStorage.riddleNumber);
     }
+    document.getElementById("charFlashDiv").style.display = "None";
     questions[riddleNumber].generateText();
+}
+
+function flashCharacter (char, delay) {
+    document.getElementById("charFlashDiv").innerHTML = char;
+    show();
+    setTimeout(hide, delay);
+}
+
+function show () {
+    document.getElementById("charFlashDiv").style.display = "";
+    document.getElementById("centredbody").style.display = "None";
+}
+
+function hide () {
+    document.getElementById("charFlashDiv").style.display = "None";
+    document.getElementById("centredbody").style.display = "";
 }
 
 function checkAnswer () {
@@ -13,154 +30,13 @@ function checkAnswer () {
         riddleNumber = questions[riddleNumber].nextQuestion;
         localStorage.riddleNumber = riddleNumber;
         questions[riddleNumber].generateText();
+        flashCharacter(hiddenSentence[riddleNumber], 500)
     } else {
         console.log("FALSE");
     }
-    document.getElementById("inputbox").value = "";
+    hide();
 }
 
-const utmanare = [
-    {
-        generateText: function () {
-            document.getElementById("questionLabel").innerHTML = "Vad är ett annat ord för gredelin?";
-            document.getElementById("inputbox").setAttribute("type", "text");
-        },
-        validateAnswer: function () {
-            return (
-                document.getElementById("inputbox")
-                        .value
-                        .toLowerCase()
-                        .match(/lila/)
-                        .length > 
-                0
-            );
-        },
-        nextQuestion: 1
-    },
-    {
-        generateText: function () {
-            document.getElementById("questionLabel").innerHTML = "Hur många trianglar? (24st)<br><img src='https://1.bp.blogspot.com/-5xv6Cg1yhFo/VNLXEhuYiXI/AAAAAAAAAXw/dNgNTrE4G5M/s1600/image002.gif' width='250'/>"
-            document.getElementById("inputbox").setAttribute("type", "text");
-        },
-        validateAnswer: function () {
-            return (
-                document.getElementById("inputbox")
-                        .value ===
-                "24"
-            );
-        },
-        nextQuestion: 2
-    },
-    {
-        generateText: function () {
-            document.getElementById("questionLabel").innerHTML = "Ursäkta, va? \"Flytta tändstickor\". Whatever that means";
-        },
-        validateAnswer: function () {
-            return true;
-        },
-        nextQuestion: 3
-    },
-    {
-        generateText: function () {
-            document.getElementById("questionLabel").innerText = "Ladda upp en bild på er patrull där ni lagar tigerkaka till era ledare!<br><a href=\"https://www.ica.se/recept/tigerkaka-719813/\">[Exempelrecept]</a>";
-            document.getElementById("inputbox").setAttribute("type", "file");
-        },
-        validateAnswer: function () {
-            const imageformats = [".png", ".jpg", "jpeg", ".gif", "tiff", ".bmp"]
-            let text = document.getElementById("inputbox").value.toString()
-            text = text.substr(text.length - 4);
-            console.log(text);
-            if (imageformats.indexOf(text) != -1) {
-                return true;
-            }
-            return false;
-        },
-        nextQuestion: 4
-    },
-    {
-        generateText: function () {
-            //Löv
-        },
-        validateAnswer: function () {
-            return true;
-        },
-        nextQuestion: 5
-    },
-    {
-        generateText: function () {
-            document.getElementById("questionLabel").innerHTML = "Hur mycket är dubbelt av hälften av 250?";
-            document.getElementById("inputbox").setAttribute("type", "text");
-        },
-        validateAnswer: function () {
-            return (
-                document.getElementById("inputbox")
-                        .value ===
-                "250" ||
-                document.getElementById("inputbox")
-                        .value ===
-                "tvåhundrafemtio"
-            );
-        },
-        nextQuestion: 6
-    },
-    {
-        generateText: function () {
-            document.getElementById("questionLabel").innerHTML = "Vilket år kom scouting till Sverige?";
-            document.getElementById("inputbox").setAttribute("type", "text");
-        },
-        validateAnswer: function () {
-            return (
-                document.getElementById("inputbox")
-                        .value ===
-                "1983" ||
-                document.getElementById("inputbox")
-                        .value ===
-                "nittonåttiotre"
-            );
-        },
-        nextQuestion: 7
-    },
-];
-
-const questions = [
-    {
-        generateText: function () {
-            document.getElementById("questionLabel").innerHTML = "Vad säger morsen?<br><audio controls><source src='assets/morse.wav' type='audio/wav'>Your browser does not support the audio element.</audio>";
-            document.getElementById("inputbox").setAttribute("type", "text");
-        },
-        validateAnswer: function () {
-            return document.getElementById("inputbox").value === "HEJ";
-        },
-        nextQuestion: 1
-    },
-    {
-        generateText: function () {
-            document.getElementById("questionLabel").innerText = "Ladda upp en bild!";
-            document.getElementById("inputbox").setAttribute("type", "file");
-        },
-        validateAnswer: function () {
-            const imageformats = [".png", ".jpg", "jpeg", ".gif", "tiff", ".bmp"]
-            let text = document.getElementById("inputbox").value.toString()
-            text = text.substr(text.length - 4);
-            console.log(text);
-            if (imageformats.indexOf(text) != -1) {
-                return true;
-            }
-            return false;
-        },
-        nextQuestion: 2
-    },
-    {
-        generateText: function () {
-            document.getElementById("questionLabel").innerHTML = "Hur många trianglar? (24st)<br><img src='https://1.bp.blogspot.com/-5xv6Cg1yhFo/VNLXEhuYiXI/AAAAAAAAAXw/dNgNTrE4G5M/s1600/image002.gif' width='250'/>";
-            document.getElementById("inputbox").setAttribute("type", "text");
-        },
-        validateAnswer: function () {
-            if (document.getElementById("inputbox").value === "24") {
-                return true;
-            }
-            return false;
-        },
-        nextQuestion: 0
-    }
-];
+function goBack () {
+    riddleNumber = Math.max(0, riddleNumber - 1); //MAKE SURE THIS LOGIC STILL APPLIES WHEN ALL QUESTIONS ARE ADDED!
+}
