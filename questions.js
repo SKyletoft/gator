261be18507cb7090c37c34514b8e573b191f7628
Samuel Kyletoft <skyletoft@gmail.com>
2019-12-31 15:46:46 +0100

    A tonne of new questions, though I broke all the leaf-questions (ON PURPOSE)

--- questions.js
+++ questions.js
@@ -1,9 +1,9 @@
 const hiddenSentence = "59°58'57.8\"N_15°26'50.2\"E";
 const questionOrder = [
-	[],
-	[],
-	[],
-	[15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13]
+	[34, 30, 17, 35, 18, null, 31, 6, 32, 8, 9, 10, 33, 12, 19, 26],
+	[27, 28, 17, 36, 18, null, 21, 6, 22, 8, 9, 10, 29, 12, 25, 26],
+	[15, 16, 17, 37, 18, null, 21, 6, 22, 8, 9, 10, 23, 12, 24, 14],
+	[15, 0, 1, 2, 3, null, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
 ];
 const questionsByGroup = [
 	questionOrder[0].length,
@@ -11,15 +11,9 @@ const questionsByGroup = [
 	questionOrder[2].length,
 	questionOrder[3].length
 ];
-const startingQuestionByGroup = [
-	questionOrder[0][0],
-	questionOrder[1][0],
-	questionOrder[2][0],
-	questionOrder[3][0]
-];
 
 const questions = [
-	{
+	{ // 0
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vad är ett annat ord för gredelin?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -29,26 +23,26 @@ const questions = [
 			return input.match(/lila/) != null;
 		}
 	},
-	{
+	{ // 1
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Hur många trianglar?<br><img src='https://1.bp.blogspot.com/-5xv6Cg1yhFo/VNLXEhuYiXI/AAAAAAAAAXw/dNgNTrE4G5M/s1600/image002.gif' width='250'/>"
 			document.getElementById("inputbox").setAttribute("type", "text");
 		},
 		validateAnswer: function () {
 			const input = lowerCaseInput();
-			return input.match(/24/) != null;
+			return input === "24";
 		}
 	},
-	{
+	{ // 2
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "<img src=\"assets/car number.png\"/>>";
 		},
 		validateAnswer: function () {
 			const input = lowerCaseInput();
-			return input.match(/87/) != null;
+			return input === "87";
 		}
 	},
-	{
+	{ // 3
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Ladda upp en bild på er patrull där ni lagar tigerkaka till era ledare!<br><a href=\"https://www.ica.se/recept/tigerkaka-719813/\">[Exempelrecept]</a>";
 			document.getElementById("inputbox").setAttribute("type", "file");
@@ -60,7 +54,7 @@ const questions = [
 			return imageformats.includes(text);
 		}
 	},
-	{
+	{ // 4
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Identifiera följande löv<br/><img src=\"assets/blad/utmanare.png\" id=\"leaves\"/>";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -86,7 +80,7 @@ const questions = [
 			return true;
 		}
 	},
-	{
+	{ // 5
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Hur mycket är dubbelt av hälften av tvåhundrafemtio?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -96,7 +90,7 @@ const questions = [
 			return input === "250" || input === "tvåhundrafemtio";
 		}
 	},
-	{
+	{ // 6
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vilket år kom scouting till Sverige?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -106,7 +100,7 @@ const questions = [
 			return input === "1910" || input === "nittontio";
 		}
 	},
-	{
+	{ // 7
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Skriv en scoutlag";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -132,7 +126,7 @@ const questions = [
 			return matches > 0;
 		}
 	},
-	{
+	{ // 8
 		generateText: function () {
 			document.getElementById("questionLabel").innerText = "Ta en \"Gäster med gester\"-bild i enighet med scoutlag #4";
 			document.getElementById("inputbox").setAttribute("type", "file");
@@ -144,7 +138,7 @@ const questions = [
 			return imageformats.includes(input);
 		}
 	},
-	{
+	{ // 9
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vad är det för symbol på taket av byggnaden 59°21’12.4”N 18°24’22.6”E";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -154,7 +148,7 @@ const questions = [
 			return input.match(/triang/) != null;
 		}
 	},
-	{
+	{ // 10
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Bättre lyss till den sträng som brast, än att aldrig ha spänt en ______";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -164,7 +158,7 @@ const questions = [
 			return input.match(/båge/) != null;
 		}
 	},
-	{
+	{ // 11
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "En man kom in på en restaurang och sa endast en bokstav. Vad beställde han?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -174,7 +168,7 @@ const questions = [
 			return input === "t";
 		}
 	},
-	{
+	{ // 12
 		generateText: function () {
 			//MUSIKKORSORD?
 			document.getElementById("questionLabel").innerHTML = "Just click next";
@@ -185,7 +179,7 @@ const questions = [
 			return true;
 		}
 	},
-	{
+	{ // 13
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "I vilket landskap ligger Kopparbo?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -195,7 +189,7 @@ const questions = [
 			return input.match(/dalarna/) != null;
 		}
 	},
-	{
+	{ // 14
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vilken är näst den vanligaste bokstaven i svenska språket?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -205,7 +199,7 @@ const questions = [
 			return input === "a";
 		}
 	},
-	{
+	{ // 15
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "<audio controls><source src='assets/morseUtmanare.wav' type='audio/wav'>Your browser does not support the audio element.<br>Does any modern browser not support audio? Kom igen, det är 2019!</audio>";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -215,7 +209,7 @@ const questions = [
 			return input === "var uppmärksam på vad som händer" || input === "var uppmarksam pa vad som hander";
 		}
 	},
-	{
+	{ // 16
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vilken fär får man av att blanda gult och rött";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -225,17 +219,17 @@ const questions = [
 			return input.match(/orange/) != null;
 		}
 	},
-	{
+	{ // 17
 		generateText: function () {
-			document.getElementById("questionLabel").innerHTML = "Hur många kvadrater?<br><img src='http://2.bp.blogspot.com/-ipkpMJ-_SiI/T_VfNdX_1TI/AAAAAAAABHY/v6MXamdWSjY/s1600/Squares+Puzzle+-+Asadkhan101.jpg' width='250'/>"
+			document.getElementById("questionLabel").innerHTML = "Hur många kvadrater?<br><img src='https://mattemarknaden.files.wordpress.com/2014/10/1016741_783962828327021_4784829287177453249_n.png' width='250'/>"
 			document.getElementById("inputbox").setAttribute("type", "text");
 		},
 		validateAnswer: function () {
 			const input = lowerCaseInput();
-			return input.match(/36/) != null;
+			return input === "8";
 		}
 	},
-	{
+	{ // 18
 		generateText: function () {
 			document.getElementById("questionLabel").innerText = "Baka en sockerkaka till era ledare och ladda upp ett foto";
 			document.getElementById("inputbox").setAttribute("type", "file");
@@ -247,10 +241,22 @@ const questions = [
 			return imageformats.includes(input);
 		}
 	},
-	{
+	{ // 19
+		generateText: function () {
+			document.getElementById("questionLabel").innerText = "Ta en bild på en tamoj!";
+			document.getElementById("inputbox").setAttribute("type", "file");
+		},
+		validateAnswer: function () {
+			const imageformats = [".png", ".jpg", "jpeg", ".gif", "tiff", ".bmp"]
+			let input = lowerCaseInput()
+			input = input.substr(input.length - 4);
+			return imageformats.includes(input);
+		}
+	},
+	{ // 20
 		//löv
 	},
-	{
+	{ // 21
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vad är PIs första åtta decimaler?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -261,10 +267,9 @@ const questions = [
 			return input === piString;
 		}
 	},
-	{
-		//Easily exploitable, I know
+	{ // 22
 		generateText: function () {
-			document.getElementById("questionLabel").innerHTML = "Skriv en scoutlag";
+			document.getElementById("questionLabel").innerHTML = "Vilka är de 14 målspåren?";
 			document.getElementById("inputbox").setAttribute("type", "text");
 		},
 		validateAnswer: function () {
@@ -294,7 +299,7 @@ const questions = [
 			return matches == laws.length;
 		}
 	},
-	{
+	{ // 23
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "En frukt- och grönsakshandlare är 54 år gammal, 175 cm lång och har 43 i skonummer? Vad väger han?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -315,7 +320,7 @@ const questions = [
 			return matches == laws.length;
 		}
 	},
-	{
+	{ // 24
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vad är Dalarnas landskapsdjur?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -325,7 +330,7 @@ const questions = [
 			return input.match(/berguv/) != null;
 		}
 	},
-	{
+	{ // 25
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vad är Dalarnas landskapblomma?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -335,29 +340,19 @@ const questions = [
 			return input.match(/blåklocka/) != null;
 		}
 	},
-	{
+	{ // 26
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vad är svenskans vanligaste bokstav?";
 			document.getElementById("inputbox").setAttribute("type", "text");
 		},
 		validateAnswer: function () {
 			const input = lowerCaseInput();
-			return input.match(/e/) != null;
+			return input == "e";
 		}
 	},
-	{
+	{ // 27
 		generateText: function () {
-			document.getElementById("questionLabel").innerHTML = "Vad är svenskans vanligaste bokstav?";
-			document.getElementById("inputbox").setAttribute("type", "text");
-		},
-		validateAnswer: function () {
-			const input = lowerCaseInput();
-			return input.match(/e/) != null;
-		}
-	},
-	{
-		generateText: function () {
-			document.getElementById("questionLabel").innerHTML = "ydu xsspbunvdp sa doow";
+			document.getElementById("questionLabel").innerHTML = "ydu xsspbunvdp sa doow (3)<br><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Retrato_de_Julio_C%C3%A9sar_%2826724093101%29.jpg/800px-Retrato_de_Julio_C%C3%A9sar_%2826724093101%29.jpg', height=350 tooltip='3'/>";
 			document.getElementById("inputbox").setAttribute("type", "text");
 		},
 		validateAnswer: function () {
@@ -365,7 +360,7 @@ const questions = [
 			return input.match(/var uppmärksam på allt/) != null;
 		}
 	},
-	{
+	{ // 28
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Vad får du om du blandar gul och blå?";
 			document.getElementById("inputbox").setAttribute("type", "text");
@@ -375,14 +370,97 @@ const questions = [
 			return input.match(/grön/) != null;
 		}
 	},
-	{
+	{ // 29
 		generateText: function () {
 			document.getElementById("questionLabel").innerHTML = "Från vilken del av Polen kommer Jultomten?";
 			document.getElementById("inputbox").setAttribute("type", "text");
 		},
 		validateAnswer: function () {
 			const input = lowerCaseInput();
-			return input.match(/nor/) != null;
+			return input.match(/nor/) != null && input.match(/polen/) != null;
+		}
+	},
+	{ // 30
+		generateText: function () {
+			document.getElementById("questionLabel").innerHTML = "Vilken färg får du om du blandar vitt och rött";
+			document.getElementById("inputbox").setAttribute("type", "text");
+		},
+		validateAnswer: function () {
+			const input = lowerCaseInput();
+			return input.match(/rosa/) != null;
+		}
+	},
+	{ // 31
+		generateText: function () {
+			document.getElementById("questionLabel").innerHTML = "Hur många scoutlagar finns det";
+			document.getElementById("inputbox").setAttribute("type", "text");
+		},
+		validateAnswer: function () {
+			const input = lowerCaseInput();
+			return input === "7";
+		}
+	},
+	{ // 32
+		generateText: function () {
+			document.getElementById("questionLabel").innerHTML = "Hur många målspår finns det inom scouterna";
+			document.getElementById("inputbox").setAttribute("type", "text");
+		},
+		validateAnswer: function () {
+			const input = lowerCaseInput();
+			return input === "14";
+		}
+	},
+	{ // 33
+		generateText: function () {
+			document.getElementById("questionLabel").innerHTML = "Hur skriver man vatten med två bokstäver?";
+			document.getElementById("inputbox").setAttribute("type", "text");
+		},
+		validateAnswer: function () {
+			const input = lowerCaseInput();
+			return input.match(/is/) != null;
+		}
+	},
+	{ // 34
+		generateText: function () {
+			document.getElementById("questionLabel").innerHTML = "<img src='assets/bradgard.png' />";
+			document.getElementById("inputbox").setAttribute("type", "text");
+		},
+		validateAnswer: function () {
+			const input = lowerCaseInput();
+			return input.match(/var uppmärksam på allt/) != null;
+		}
+	},
+	{ // 35
+		generateText: function () {
+			document.getElementById("questionLabel").innerHTML = "Vilken färg har himlen när du står på månen?";
+			document.getElementById("inputbox").setAttribute("type", "text");
+		},
+		validateAnswer: function () {
+			const input = lowerCaseInput();
+			return input.match(/svart/) != null;
+		}
+	},
+	{ // 36
+		generateText: function () {
+			document.getElementById("questionLabel").innerHTML = "10 guldfiskar i en skål, två dör, tre simmar iväg, 4 drunknar. Hur många fiskar är det kvar i skålen?";
+			document.getElementById("inputbox").setAttribute("type", "text");
+		},
+		validateAnswer: function () {
+			const input = lowerCaseInput();
+			return input === "10";
+		}
+	},
+	{ // 37
+		generateText: function () {
+			document.getElementById("questionLabel").innerHTML = "<img src='https://www.humorbibeln.se/wp-content/uploads/sites/4/2019/10/visning9mistag.jpg' />";
+			document.getElementById("inputbox").setAttribute("type", "text");
+		},
+		validateAnswer: function () {
+			const input = lowerCaseInput();
+			return input.match(/mistaget/) != null ||
+				input.match(/misstaget/) != null ||
+				input.match(/ s/) != null ||
+				input.match(/"s"/) != null;
 		}
 	}
 ];
