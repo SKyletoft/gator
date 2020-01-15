4f0aa275a9c73cfb406ba8fe1dbad4a66b1c4eaa
Samuel Kyletoft <skyletoft@gmail.com>
2020-01-15 09:50:48 +0100

    Chat test

--- chat.js
+++ chat.js
@@ -4,6 +4,11 @@ const chat = [
     "[undefined] I've found something important.",
     "[undefined] This changes EVERYTHING",
     "[me] what?",
+    "[undefined] I found the body",
+    "[undefined] It was in",
+    "null has joined the chat",
+    "[null] Hi!",
+    "[undefined] Never mind then"
 ];
 const delays = [
     0,
@@ -11,24 +16,27 @@ const delays = [
     5000,
     1000,
     10000,
-    10000,
-    1000,
-    1000
+    2000,
+    3000,
+    4000
 ];
 
 let index = 0;
+let latestmessage;
+
 function printChat () {
-    console.log(Date.now() + " " + chat[index]);
+    let d = new Date();
+    console.log(d.getHours() + ":" + d.getMinutes() + " " + chat[index]);
     index++;
 }
 
 function startChat() {
     index = 0;
-    let latestmessage = Date.now();
-    while (index < chat.length) {
+    latestmessage = Date.now();
+    setInterval(() => {
         if (Date.now() > latestmessage + delays[index]) {
             printChat();
             latestmessage = Date.now();
         }
-    }
+    }, 500);
 }
